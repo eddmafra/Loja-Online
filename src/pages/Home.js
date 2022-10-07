57b940d03f793b0d4c5597ca75f3ce7742443211
Karla Oshikawa <105741647+karlaoshikawa@users.noreply.github.com>
2022-10-08 08:58:06 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Search from '../components/Search';
 import { Link } from 'react-router-dom';
+import Search from '../components/Search';
 
 class Home extends React.Component {
   render() {
