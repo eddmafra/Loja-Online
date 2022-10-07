2e782648a5feacb2fc0511a1ed48f4a46c4e3735
Karla Oshikawa <karlaoshikawa@iMac-de-Karla.local>
2022-10-08 08:49:42 +0900

    Requisito 5 - Renan, Karla, Edu, Otho, Rafaela

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Search from '../components/Search';
 
 class Home extends React.Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Search />
+      </div>
     );
   }
 }
