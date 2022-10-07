f3b7ea5b5aa3773387298c699ac969d6702aa5ee
Rafaela <rafaela.queiroz12@hotmail.com>
2022-10-07 18:40:38 -0300

    Requito 3 - Rafaela

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import './App.css';
 import * as api from './services/api';
 
@@ -11,6 +12,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link to="/cart" data-testid="shopping-cart-button" />
+        <button type="button">
+          Carrinho de Compras
+        </button>
+      </>
     );
   }
 }
