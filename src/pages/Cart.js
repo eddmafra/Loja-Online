afa80bb03738c24d0209991b5d0e1a4ea0d2da79
Rafaela <rafaela.queiroz12@hotmail.com>
2022-10-11 18:21:27 -0300

    Requisito 8 - Rafaela e Karla

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Cart extends React.Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    const getLocalStorage = localStorage.getItem('listCart');
+    const returnGet = JSON.parse(getLocalStorage);
+    this.setState({
+      products: returnGet,
+    });
+  }
+
   render() {
+    const { products } = this.state;
     return (
-      <p data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
-      </p>
+      <>
+        <Link to="/">Home</Link>
+        {
+          products.length === 0
+            && <p data-testid="shopping-cart-empty-message"> Seu carrinho está vazio</p>
+        }
+        { products.length > 0 && (
+          products.map((item, index) => (
+            <section key={ index }>
+              <p data-testid="shopping-cart-product-name">{item.title}</p>
+              <img src={ item.thumbnail } alt={ item.title } />
+              <p>
+                R$
+                {item.price}
+
+              </p>
+              <p data-testid="shopping-cart-product-quantity">
+                {products.filter((product) => product.id === item.id).length}
+              </p>
+            </section>))
+        )}
+      </>
+
     );
   }
 }
+
 export default Cart;
