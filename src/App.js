d9a0763337296e4fa8514e75dd95f41c3acc0964
Eddmafra <eduardo_mafra@hotmail.com>
2022-10-11 19:04:05 -0300

    inicio requisito 11

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import ProductDetails from './pages/ProductDetails';
 import './App.css';
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
@@ -12,7 +13,11 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/cart" component={ Cart } />
+<<<<<<< HEAD
           <Route path="/details/:id" component={ ProductDetails } />
+=======
+          <Route path="/star" component={ Form } />
+>>>>>>> ad45412 (inicio requisito 11)
         </Switch>
       </BrowserRouter>
     );
